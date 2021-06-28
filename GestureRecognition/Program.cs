890326228135cc80eec8b8fc250d0e0d86e58de7
cordea <cordear@outlook.com>
2021-06-28 16:00:33 +0800

    添加项目文件。

---
+++ GestureRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestureRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
