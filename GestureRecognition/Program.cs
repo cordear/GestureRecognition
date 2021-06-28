b2e87979088bbfd1d1baa9119e4f824ef6f97d1a
cordea <cordear@outlook.com>
2021-06-28 21:28:39 +0800

    gesture contours detection & code cleanup

--- GestureRecognition/Program.cs
+++ GestureRecognition/Program.cs
@@ -7,7 +7,7 @@ namespace GestureRecognition
     {
         private static void Main(string[] args)
         {
-            int i = 0;
+            var i = 0;
             var video = new VideoCapture();
             video.Open(0);
             if (!video.IsOpened())
@@ -26,8 +26,10 @@ namespace GestureRecognition
                 //Cv2.CvtColor(camera, grayMat, ColorConversionCodes.RGB2BGR);
                 //Cv2.Canny(grayMat, camera, 100, 200);
                 var skin = SkinDetect(camera);
-                Cv2.CvtColor(skin,skin,ColorConversionCodes.GRAY2BGR);
-                Cv2.BitwiseAnd(camera,skin,camera);
+                var contours = FindContours(skin);
+                Cv2.CvtColor(skin, skin, ColorConversionCodes.GRAY2BGR);
+                Cv2.BitwiseAnd(camera, skin, camera);
+                Cv2.DrawContours(camera, contours, -1, Scalar.Red);
                 var cameraWindow = new Window("cameraSteam", camera, WindowFlags.AutoSize);
                 var backgroundWindow = new Window("background", skin, WindowFlags.AutoSize);
                 Cv2.WaitKey(10);
@@ -37,6 +39,7 @@ namespace GestureRecognition
                     GC.Collect();
                 }
             }
+
             camera.Release();
             Cv2.DestroyAllWindows();
         }
@@ -44,12 +47,19 @@ namespace GestureRecognition
         public static Mat SkinDetect(Mat input)
         {
             var output = new Mat();
-            Cv2.CvtColor(input,output,ColorConversionCodes.BGR2YCrCb);
-            Cv2.Split(output, out Mat[] frames);
+            Cv2.CvtColor(input, output, ColorConversionCodes.BGR2YCrCb);
+            Cv2.Split(output, out var frames);
             var crFrame = frames[1];
-            Cv2.GaussianBlur(crFrame,crFrame,new Size(5,5),0);
+            Cv2.GaussianBlur(crFrame, crFrame, new Size(5, 5), 0);
             Cv2.Threshold(crFrame, crFrame, 0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
             return crFrame;
+        }
+
+        public static Point[][] FindContours(Mat input)
+        {
+            Cv2.FindContours(input, out var contours, out var hierarchyIndices, RetrievalModes.Tree,
+                ContourApproximationModes.ApproxSimple);
+            return contours;
         }
     }
 }
