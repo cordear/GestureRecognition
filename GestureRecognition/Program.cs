6f485170cedeefed59594fd027a36c363bb7cb71
cordea <cordear@outlook.com>
2021-06-28 21:04:43 +0800

    Change to skin detection

--- GestureRecognition/Program.cs
+++ GestureRecognition/Program.cs
@@ -7,6 +7,7 @@ namespace GestureRecognition
     {
         private static void Main(string[] args)
         {
+            int i = 0;
             var video = new VideoCapture();
             video.Open(0);
             if (!video.IsOpened())
@@ -17,27 +18,38 @@ namespace GestureRecognition
 
             Console.WriteLine("camera open success.");
             var camera = new Mat();
-            var grayMat = new Mat();
-            var background = new Mat();
-            var es = Cv2.GetStructuringElement(MorphShapes.Ellipse, new Size(3, 3));
-            var mog = BackgroundSubtractorMOG2.Create();
             while (true)
             {
+                i++;
                 video.Read(camera);
                 if (camera.Empty()) break;
                 //Cv2.CvtColor(camera, grayMat, ColorConversionCodes.RGB2BGR);
                 //Cv2.Canny(grayMat, camera, 100, 200);
-                mog.Apply(camera,background);
-                Cv2.Threshold(background, background, 244, 255, ThresholdTypes.Binary);
-                Cv2.Dilate(background,background,es,iterations:2);
-                Cv2.CvtColor(background,background,ColorConversionCodes.GRAY2BGR);
-                Cv2.BitwiseAnd(camera,background,camera);
+                var skin = SkinDetect(camera);
+                Cv2.CvtColor(skin,skin,ColorConversionCodes.GRAY2BGR);
+                Cv2.BitwiseAnd(camera,skin,camera);
                 var cameraWindow = new Window("cameraSteam", camera, WindowFlags.AutoSize);
-                var backgroundWindow = new Window("background", background, WindowFlags.AutoSize);
+                var backgroundWindow = new Window("background", skin, WindowFlags.AutoSize);
                 Cv2.WaitKey(10);
+                if (i >= 20)
+                {
+                    i = 0;
+                    GC.Collect();
+                }
             }
             camera.Release();
             Cv2.DestroyAllWindows();
+        }
+
+        public static Mat SkinDetect(Mat input)
+        {
+            var output = new Mat();
+            Cv2.CvtColor(input,output,ColorConversionCodes.BGR2YCrCb);
+            Cv2.Split(output, out Mat[] frames);
+            var crFrame = frames[1];
+            Cv2.GaussianBlur(crFrame,crFrame,new Size(5,5),0);
+            Cv2.Threshold(crFrame, crFrame, 0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+            return crFrame;
         }
     }
 }
