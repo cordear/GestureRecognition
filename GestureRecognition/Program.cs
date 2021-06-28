e75acb0c7b637c4180b069c2f05267ed8d1d7bca
cordea <cordear@outlook.com>
2021-06-28 17:23:05 +0800

    Add tentative background subtractor for camera

--- GestureRecognition/Program.cs
+++ GestureRecognition/Program.cs
@@ -18,15 +18,26 @@ namespace GestureRecognition
             Console.WriteLine("camera open success.");
             var camera = new Mat();
             var grayMat = new Mat();
+            var background = new Mat();
+            var es = Cv2.GetStructuringElement(MorphShapes.Ellipse, new Size(3, 3));
+            var mog = BackgroundSubtractorMOG2.Create();
             while (true)
             {
                 video.Read(camera);
                 if (camera.Empty()) break;
-                Cv2.CvtColor(camera, grayMat, ColorConversionCodes.RGB2BGR);
-                Cv2.Canny(grayMat, camera, 100, 200);
+                //Cv2.CvtColor(camera, grayMat, ColorConversionCodes.RGB2BGR);
+                //Cv2.Canny(grayMat, camera, 100, 200);
+                mog.Apply(camera,background);
+                Cv2.Threshold(background, background, 244, 255, ThresholdTypes.Binary);
+                Cv2.Dilate(background,background,es,iterations:2);
+                Cv2.CvtColor(background,background,ColorConversionCodes.GRAY2BGR);
+                Cv2.BitwiseAnd(camera,background,camera);
                 var cameraWindow = new Window("cameraSteam", camera, WindowFlags.AutoSize);
+                var backgroundWindow = new Window("background", background, WindowFlags.AutoSize);
                 Cv2.WaitKey(10);
             }
+            camera.Release();
+            Cv2.DestroyAllWindows();
         }
     }
 }
