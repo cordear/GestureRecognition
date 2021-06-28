13fe8852af72672ede352bacb96dbd9306d26f0f
cordea <cordear@outlook.com>
2021-06-28 16:25:43 +0800

    Add basic camera invoke function

--- GestureRecognition/Program.cs
+++ GestureRecognition/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml;
+using OpenCvSharp;
 
 namespace GestureRecognition
 {
@@ -7,6 +9,29 @@ namespace GestureRecognition
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var video = new VideoCapture();
+            video.Open(0);
+            if (!video.IsOpened())
+            {
+                Console.WriteLine("camera open failed.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("camera open success.");
+            }
+            Mat camera = new Mat();
+            while (true)
+            {
+                video.Read(camera);
+                if (camera.Empty())
+                {
+                    break;
+                }
+
+                var cameraWindow = new Window("cameraSteam",camera,WindowFlags.AutoSize);
+                Cv2.WaitKey(10);
+            }
         }
     }
 }
